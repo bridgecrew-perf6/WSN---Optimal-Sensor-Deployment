3f3f3c04d257742c43c6b3a494ae6bab22cad909
Rohit Goyal <sprkgoyal@gmail.com>
2022-03-15 10:53:13 +0530

    added TourConstruction

--- rohit.goyal/TourConstruction.py
+++ rohit.goyal/TourConstruction.py
@@ -32,17 +32,70 @@ def find_Target_under(D, T):
                 Target_under[d].append(t)
 
 
-def find_N_ifull(node_st, node_points):
-    pass
+def find_N_ifull(S_k, node_points):
+    
+    def dist(a, b):
+        return np.sqrt(np.power(a, 2) + np.power(b, 2))
 
-def find_N_eff(node_st, node_points):
-    pass
+    N_ifull = []
+    
+    for node1 in S_k:
+        for node in node_points:
+            if dist(node, node1) <= r_c:
+                N_ifull.append(node)
 
-def find_coverage_gain(N):
-    pass
+    return N_ifull
 
-def next_best_node(d_o, tau):
-    pass
+
+def find_N_ieff(node_points, T_cov):
+    
+    N_ieff = set()
+    T_cov_tmp = T_cov.copy()
+
+    for node in node_points:
+        for target in Target_under[node]:
+            if target not in T_cov_tmp:
+                T_cov_tmp.append(target)
+                N_ieff.add(node)
+    
+    return list(N_ieff)
+
+
+def find_coverage_gain(N, T_cov):
+    
+    ret = 0
+    for node in N:
+        for target in Target_under[node]:
+            if target not in T_cov:
+                ret += 1
+
+    return ret
+
+
+def next_best_node(d_o, tau, N_i):
+    
+    phermone_vals = defaultdict(lambda :0)
+    
+    def dist(a, b):
+        return np.sqrt(np.power(a, 2) + np.power(b, 2))
+
+    deno_sum = 0
+    for node in N_i:
+        eta = 1 / dist(d_o, node)
+        deno_sum += np.power(tau[d_o][node], alpha) * np.power(eta, beta)
+
+    for node in N_i:
+        eta = 1 / dist(d_o, node)
+        phermone_vals[node] = (np.power(tau[d_o][node], alpha) * np.power(eta, beta)) / deno_sum
+
+    ret = [0, 0]
+
+    for node, p_val in phermone_vals.items():
+        if p_val > phermone_vals[ret]:
+            ret = node
+
+    return ret
+
 
 def Tour_Construction(ant_num, D, T, d_o, R_min, tau_mat):
     '''
@@ -72,15 +125,15 @@ def Tour_Construction(ant_num, D, T, d_o, R_min, tau_mat):
 
         # covering all target points
         while not equal(T_cov, T):
-            N_ifull = find_N_ifull(d_o, D_minus)
-            N_ieff = find_N_eff(d_o, N_ifull)
+            N_ifull = find_N_ifull(S_k, D_minus)
+            N_ieff = find_N_ieff(N_ifull, T_cov)
             N_i = N_ieff if len(N_ieff) != 0 else N_ifull
-            converage_gain_i = find_coverage_gain(N_i)
+            converage_gain_i = find_coverage_gain(N_i, T_cov)
             S_k.append(d_o)
             for target in Target_under[d_o]:
                 if target not in T_cov:
                     T_cov.append(target)
-            d_o = next_best_node(d_o, tau_mat)
+            d_o = next_best_node(d_o, tau_mat, N_i)
         
         # Updating the variables after covering all arget points
         S.append(S_k)
