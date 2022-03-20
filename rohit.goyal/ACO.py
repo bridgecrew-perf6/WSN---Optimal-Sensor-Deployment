cc38aa00766807fdb6bf781514afd8e37a051433
Rohit Goyal <sprkgoyal@gmail.com>
2022-03-20 12:26:03 +0530

    done line 1-9

--- rohit.goyal/ACO.py
+++ rohit.goyal/ACO.py
@@ -1,3 +1,4 @@
+from cv2 import FILE_NODE_UNIFORM
 import numpy as np
 import os
 from dotenv import load_dotenv
@@ -12,13 +13,36 @@ r_c = os.load_dotenv('NODE_RANGE')
 Reliability = TourConstruction.Reliability
 
 def AntColonyOptimization(D, T, d_o, R_min):
-    
-    S = []
-    R_S = 0
-    k = 0
-    D_minus = D.copy()
-    
-    while R_S < R_min:
-        R_S = Reliability(S)
+	
+	S = []
+	R_S = 0
+	k = 0
+	D_minus = D.copy()
+	
+	while R_S < R_min:
+		k = k+1
+		S_k = []
+		T_cov = []
 
-    return S, R_S
+		while TourConstruction.equal(T_cov, T):
+			
+			dist = lambda a, b: np.sqrt(np.square(a[0] - b[0]) + np.square(a[1] - b[1]))
+			N_next = []
+			
+			if len(S_k) == 0:
+				for d in D_minus:
+					if dist(d, d_o) < r_c:
+						N_next.append(d)
+			
+			else:
+				for dd in S_k:
+					for d in D_minus:
+						if dist(d, dd) < r_c and d not in S_k:
+							N_next.append(d)
+
+			
+
+		R_S = Reliability(S)
+
+
+	return S, R_S
